1677b86e6d668d41a2cf3feb2f7fde99a9b4c7e1
Kotov Oleg <grum1972@gmail.com>
2022-07-22 17:01:43 +0300

    6.0.1. Объявление массива. [#3613 #192862]

---
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -0,0 +1,9 @@
+package ru.job4j.array;
+
+public class ArrayDefinition {
+    public static void main(String[] args) {
+        short[] ages = new short[100];
+        String[] surnames = new String[10550];
+        float[] prices = new float[40];
+    }
+}
